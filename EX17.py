84962ed16954a41f0a43a175a262343e11034808
Akinfiresoye -Victor <eninlavictor30@gmail.com>
2025-12-24 11:15:40 +0100

    Add files via upload

---
+++ EX17.py
@@ -0,0 +1,27 @@
+from sys import argv
+from os.path import exists
+
+# Assuming you are passing these as arguments or defining them
+from_file = "test.txt"
+to_file = "new_test.txt"
+
+print(f"Copying from {from_file} to {to_file}")
+
+# We could do these two on one line like this: indata = open(from_file).read()
+in_file = open(from_file)
+indata = in_file.read()
+
+print(f"The input file is {len(indata)} bytes long")
+
+print(f"Does the output file exist? {exists(to_file)}")
+print("Ready, hit RETURN to continue, CTRL-C to abort.")
+input()
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+print("Alright, all done.")
+
+out_file.close()
+in_file.close()
+#en
