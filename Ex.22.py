f14eb4d7179a163c25a5b290c98adfed6e438b5e
praise-akintomide <praiseetioloja@gmail.com>
2025-12-24 13:27:33 +0100

    Add files via upload

---
+++ Ex.22.py
@@ -0,0 +1,20 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> fruit = [
+...     ['Apples', 12, 'AAA'], ['Oranges', 1, 'B'],
+...     ['Pears', 2, 'A'], ['Grapes', 14, 'UR']]
+...  
+...   cars = [
+...      ['Cadillac', ['Black', 'Big', 34500]],
+...      ['Corvette', ['Red', 'Little', 1000000]],
+...      ['Ford', ['Blue', 'Medium', 1234]],
+...      ['BMW', ['White', 'Baby', 7890]]
+...  ]
+... 
+...  languages = [
+...      ['Python', ['Slow', ['Terrible', 'Mush']]],
+...      ['JavaSCript', ['Moderate', ['Alright', 'Bizarre']]]
+...      ['Perl6', ['Moderate', ['Fun', 'Weird']]],
+...      ['C', ['Fast', ['Annoying', 'Dangerous']]],
+...      ['Forth', ['Fast', ['Fun', 'Difficult']]],
+...  ]
---
+++ Ex.24.py
@@ -0,0 +1,23 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> fruit = [
+...     {"kind": "Apples",  "count": 12, "rating": "AAA"},
+...     {"kind": "Oranges", "count": 1,  "rating": "B"},
+...     {"kind": "Pears",   "count": 2,  "rating": "A"},
+...     {"kind": "Grapes",  "count": 14, "rating": "UR"}
+... ];
+... 
+... cars = [
+...     {"type": "Cadillac", "color": "Black", "size": "Big",    "miles": 100000},
+...     {"type": "Corvette", "color": "Red",   "size": "Little", "miles": 5000},
+...     {"type": "Ford",     "color": "Blue",  "size": "Medium", "miles": 30000},
+...     {"type": "BMW",      "color": "White", "size": "Baby",   "miles": 7000}
+... ];
+... 
+... languages = [
+...     {"name": "Python",     "speed": "Slow",     "opinion": ["Terrible"]},
+...     {"name": "JavaScript", "speed": "Moderate", "opinion": ["Awesome"]},
+...     {"name": "Perl6",      "speed": "Moderate", "opinion": ["Fun"]},
+...     {"name": "C",          "speed": "Fast",     "opinion": ["Annoying", "Dangerous"]},
+...     {"name": "Forth",      "speed": "Fast",     "opinion": ["Fun", "Different"]}
+... ];
